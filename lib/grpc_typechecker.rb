eda95d54e319d0ac6b435bf096e95035b06b1c73
Masayuki Mizuno <mizuno@wantedly.com>
2020-12-10 19:30:12 +0900

    Create a gem

---
+++ lib/grpc_typechecker.rb
@@ -0,0 +1,6 @@
+require "grpc_typechecker/version"
+
+module GrpcTypechecker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/grpc_typechecker/version.rb
@@ -0,0 +1,3 @@
+module GrpcTypechecker
+  VERSION = "0.1.0"
+end
